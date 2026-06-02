782be9d0a294fa9435c6efa969fb7ececd31f3cc
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-02 19:28:50 +0530

    Create web_server.py

---
+++ plugins/web_server.py
@@ -0,0 +1,7 @@
+from aiohttp import web
+async def web_server():
+    app = web.Application()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
