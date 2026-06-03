85e1fc5c15eccbe005486f3ebf9103e39a6f73d3
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-03 21:22:48 +0530

    Delete plugins/start.py

--- plugins/start.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-@Client.on_message(filters.command('start') & filters.private)
-async def start_command(client: Client, message: Message):
-    await message.reply_text("**Hello! Bot is working!** ✅")
