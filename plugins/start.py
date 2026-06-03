e60da7000af39006aa145756f536925001d3b2f8
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-03 20:25:06 +0530

    Refactor start command registration
    Refactor start command to use a register function.

--- plugins/start.py
+++ plugins/start.py
@@ -1,9 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from bot import Bot
 
-app = Bot()
-
-@app.on_message(filters.command('start') & filters.private)
-async def start_command(client, message: Message):
-    await message.reply_text("**Hello! Bot is working!** ✅")
+def register(app):
+    @app.on_message(filters.command('start') & filters.private)
+    async def start_command(client, message: Message):
+        await message.reply_text("**Hello! Bot is working!** ✅")
