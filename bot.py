6f03bd5fbaf429c6c594f293b5c9b28d23a05601
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-03 09:15:50 +0530

    Comment out sys.exit() in exception handling
    Comment out sys.exit() to prevent immediate termination of the bot.

--- bot.py
+++ bot.py
@@ -90,7 +90,7 @@ class Bot(Client):
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
             self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
-            sys.exit()
+            #sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
