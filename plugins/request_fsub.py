66b6b8c3123c6d12dd6ca82599f165f53246f0c4
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-03 12:39:10 +0530

    Update request_fsub.py

--- plugins/request_fsub.py
+++ plugins/request_fsub.py
@@ -21,7 +21,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Invit
 from bot import Bot
 from config import *
 from helper_func import *
-from database.database import *
+from database import *
 
 # Don't Remove Credit @CodeFlix_Bots, @rohit_1888
 # Ask Doubt on telegram @CodeflixSupport
