3ba82c92d50925d21ac5d9bfb440e022bd19cbfc
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-03 15:48:44 +0530

    Update start command to include user mention

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ from database import *
 async def start_command(client: Bot, message: Message):
     await message.reply_text(
         text=START_MSG.format(
-            first=message.from_user.first_name
-        ),
+    mention=message.from_user.mention
+),
         disable_web_page_preview=True
     )
