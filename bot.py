42e1d991e42e8e1c7d53015dc3fff44f1545d7cd
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-03 10:29:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ class Bot(Client):
             api_hash=API_HASH,
             api_id=APP_ID,
             plugins={
-                "root": "plugins"
+                "root": "."
             },
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN
