6ebb2774ce6183e395feab3af362637ddcacf894
yugalcharpe753-svg <yugalcharpe753@gmail.com>
2026-06-03 20:46:22 +0530

    Refactor start command to use Client decorator

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,6 @@
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import Message
 
-def register(app):
-    @app.on_message(filters.command('start') & filters.private)
-    async def start_command(client, message: Message):
-        await message.reply_text("**Hello! Bot is working!** ✅")
+@Client.on_message(filters.command('start') & filters.private)
+async def start_command(client: Client, message: Message):
+    await message.reply_text("**Hello! Bot is working!** ✅")
